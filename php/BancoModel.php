ebc4b070914051b899af5c85243735263f6368fd
Bruno de Assunção <Brunodeassuncao03@hotmail.com>
2023-10-26 11:02:15 -0300

    Mudanças para implementação do banco de dados

---
+++ php/BancoModel.php
@@ -0,0 +1,25 @@
+<?php
+
+  class Banco {
+
+    public $id;
+    public $nome;
+    public $email;
+    public $Senha;
+   
+    
+    //vou fazer depois dos problemas principais estiverem resolvidos
+    // public $token;
+
+  }
+
+  interface ClienteDAOInterface {
+
+
+    public function Const_cliente($cliente);
+    public function criar_clie(Cliente $cliente);
+    public function deletar_clie(Cliente $id);
+    public function editar_cliente(Cliente $cliente);
+    public function findByEmail($email);
+
+  }
